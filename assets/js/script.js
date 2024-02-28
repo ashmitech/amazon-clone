96dd09786095d69feddacf7f82f6350c2d8b6312
Mahesh Sharma <mahesh.ddbank@gmail.com>
2024-02-28 21:42:26 +0545

    Added js codes and js script to index.html

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+console.log("Welcome to Amazon Clone!");
+work="It helps to know the concept of webiste design in Amazon";
+console.log(work);
+
+const student={
+    name:"Mahesh Sharma",
+    faculty: "BSC IT",
+    cpga: 3.27,
+    isPassed: true,
+};
+console.log(student);
+// typeof student
+student["name"]="Ashmita Shiwakoti";
